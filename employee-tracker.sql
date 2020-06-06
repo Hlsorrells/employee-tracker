b0c18afeeb7e11198492070d4157fd1a0d949245
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-06-06 12:06:23 -0400

    create employeeTracker_db

---
+++ employee-tracker.sql
@@ -0,0 +1,8 @@
+-- Drops the employeeTracker_db if it exists currently --
+DROP DATABASE IF EXISTS employeeTracker_db;
+
+-- Creates the "employeeTracker_db" database --
+CREATE DATABASE employeeTracker_db;
+
+-- Make it so all of the following code will affect employeeTracker_db --
+USE employeeTracker_db;
