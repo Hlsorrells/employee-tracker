5be40f9479ef381315fb94cae95b29f44f0361b7
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-06-06 12:47:20 -0400

    renamed file to reflect schema creation only

---
+++ employeeTrackerSchema.sql
@@ -0,0 +1,41 @@
+-- Drops the employeeTracker_db if it exists currently --
+DROP DATABASE IF EXISTS employeeTracker_db;
+
+-- Creates the "employeeTracker_db" database --
+CREATE DATABASE employeeTracker_db;
+
+-- Make it so all of the following code will affect employeeTracker_db --
+USE employeeTracker_db;
+
+-- Creates the table "department" within employeeTracker_db --
+CREATE TABLE department (
+id TINYINT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL
+);
+
+-- Creates the table "role" within employeeTracker_db --
+CREATE TABLE role (
+  id TINYINT AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id TINYINT,
+  -- Primary Key for Role table
+  PRIMARY KEY(id),
+  -- Foriegn Key to Department Table
+  FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+-- Creates the table "employee" within employeeTracker_db --
+CREATE TABLE employee (
+    id SMALLINT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id SMALLINT,
+    role_id TINYINT,
+    -- Primary Key for Employee Table
+    PRIMARY KEY(id),    
+    -- Self-Referencing Key to Employee Table
+    FOREIGN KEY(manager_id) REFERENCES employee(id),
+    -- Foriegn Key to Role Table
+    FOREIGN KEY(role_id) REFERENCES role(id)
+);
