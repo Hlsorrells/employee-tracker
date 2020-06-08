7fc185ec23f79d718bc5530d13dffcf2a5acb689
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-06-07 21:18:23 -0400

    created functions to create dynamic department list & to add new roles to role table using dynamic department list function

--- employeeTracker.js
+++ employeeTracker.js
@@ -19,14 +19,14 @@ connection.connect(err => {
     start();
 });
 
-// Prompts the user for action
+// Prompts the start menu
 function start() {
     inquirer
         .prompt({
             name: "operationSelection",
             type: "list",
             message: "What would you like to do?",
-            choices: ["View All Employees", "View All Departments", "View All Roles", "Create a New Department", "EXIT"]
+            choices: ["View All Employees", "View All Departments", "View All Roles", "Create a New Department", "Create a New Role", "EXIT"]
         })
         .then(answer => {
             // Based on their answer, call the appropriate function when finished prompting
@@ -42,11 +42,14 @@ function start() {
             else if (answer.operationSelection === "Create a New Department") {
                 addDepartment();
             }
+            else if (answer.operationSelection === "Create a New Role") {
+                addRole();
+            }
             else {
                 connection.end();
             }
         });
-};
+};// end of start() fct def
 
 // View all of the employees joined with role and department table information
 function viewAllEmployees() {
@@ -60,7 +63,7 @@ function viewAllEmployees() {
             start();
         }
     );
-};
+};// end of viewAllEmployees() fct def
 
 // View all the departments in the department table of the db
 function viewAllDepartments() {
@@ -74,7 +77,7 @@ function viewAllDepartments() {
             start();
         }
     );
-};
+};// end of viewAllDepartments() fct def
 
 // View all the roles joined with department name from the department table of db
 function viewAllRoles() {
@@ -88,7 +91,7 @@ function viewAllRoles() {
             start();
         }
     );
-};
+};// end of viewAllRoles() fct def
 
 // Add a new department to the department table in db
 function addDepartment() {
@@ -114,7 +117,65 @@ function addDepartment() {
                 }
             );
         });
-}
+};// end of addDepartment() fct def
+
+// Global variable to contain dynamic department list
+const deptList = [];
+
+// Retrieve department list including id field
+async function departmentList() {
+    connection.query(
+        "SELECT id, dept_name FROM department",
+        (err, res) => {
+            if (err) throw err;
+            for (let i = 0; i < res.length; i++) {
+                deptList.push(`${res[i].id}) ${res[i].dept_name}`);
+            }
+        }
+    )
+};//end departmentList() fct def
+
+// Add a new role to the role table in db
+async function addRole() {
+    // Prompt user for new role information
+    departmentList()
+    inquirer
+        .prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the title of the new role?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary for the new position?"
+            },
+            {
+                name: "department",
+                type: "list",
+                message: "Which department will be assigned the new role?",
+                choices: deptList
+            }
+        ])//end .prompt()
+        .then(answer => {
+            let title = answer.title
+            let salary = answer.salary
+            let deptID = parseInt(answer.department.split(")")[0])
+            let deptName = answer.department.split(") ")[1]
+            // Insert new department name into department table
+            connection.query(
+                "INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)",
+                [title, salary, deptID],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log(`The role of ${title} with a salary of ${salary} has been added to the ${deptName} department.`);
+                    // Re-prompt the user for next action
+                    start();
+                }
+            );
+        })
+};// end addRole() fct def
 
 // Capitalization of each word in a string
 function capitalization(str) {
@@ -123,4 +184,4 @@ function capitalization(str) {
         word => word.charAt(0).toUpperCase() + word.substring(1)
     )
     return capWords.join(" ")
-}
+}// end of capitalization() fct def
